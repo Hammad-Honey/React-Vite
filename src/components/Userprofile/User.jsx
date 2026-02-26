6b78c17b59e5e58d71ea1e5e46f478ac51043dd1
hammad <hammadpk@proton.me>
2026-02-26 13:56:49 +0500

    useEffect

--- src/components/Userprofile/User.jsx
+++ src/components/Userprofile/User.jsx
@@ -2,56 +2,56 @@ import { use, useState } from "react";
 import UserDetails from "./UserDetails";
 
 
+const UserHeight = () => {
+
+    return <>This is user height </>
+}
 
 function User() {
     const [showDetails, setShowDetails] = useState(false);
     const [userHeight, setHeight] = useState(0);
     const [userWeight, setWeight] = useState(0);
 
-    const handleHeightIncrement=()=> setHeight(userHeight+1);
-    const onHeightReset=()=> setHeight(0);
-    const onWeightReset=()=> setWeight(0);
-    const handleWeightIncrement=()=> setWeight(userWeight+1);
+    const handleHeightIncrement= ()=>{setHeight(userHeight+10)}
+
     return (
         <>
             {/* Condition for show User*/}
-            <div><button onClick={()=>setShowDetails(!showDetails)}>Show User Details</button></div>
-            {showDetails ? 
-            <>
-                <UserDetails person={{name:"X", age:30}} height={userHeight} weight={userWeight} setHeight={setHeight} setWeight={setWeight}/>
-            </>:<>
-                <div>User Details Are hidden</div>
-            </>}
-
-            {/* Condition for show height*/}
-            {/* <button onClick={() => setShowDetails(!showDetails)} >Toggle show details</button>
+            <button onClick={() => setShowDetails(!showDetails)} >Toggle show details</button>
             {showDetails ?
                 <UserDetails person={{ name: "Hammad", age: 26, height: { userHeight }, weight: { userWeight } }} />
                 :
                 <UserHeight />
-            } */}
+            }
+
+            {/* Condition for show height*/}
+            <button onClick={() => setShowDetails(!showDetails)} >Toggle show details</button>
+            {showDetails ?
+                <UserDetails person={{ name: "Hammad", age: 26, height: { userHeight }, weight: { userWeight } }} />
+                :
+                <UserHeight />
+            }
 
 
 
 
             {/* Condition for show weight*/}
-            {/* <button onClick={() => setShowDetails(!showDetails)} >Toggle show details</button> */}
-            {/* {showDetails ?
+            <button onClick={() => setShowDetails(!showDetails)} >Toggle show details</button>
+            {showDetails ?
                 <UserDetails person={{ name: "Hammad", age: 26, height: { userHeight }, weight: { userWeight } }} />
                 :
                 <UserHeight />
-            } */}
-            {/* <UserDetails onHeightIncrement={handleHeightIncrement} />;
-            <UserDetails onHeightReset={handleHeightReset} />;
-            <UserDetails onWeightIncrement={handleWeightIncrement} />;
-            <UserDetails onWeightReset={handleWeightReset} />; */}
+            }
 
 
         </>
 
 
     )
-
+    return <Child onIncrement={handleHeightIncrement} />;
+    return <Child onIncrement={handleHeightReset} />;
+    return <Child onIncrement={handleWeightIncrement} />;
+    return <Child onIncrement={handleWeightReset} />;
 
 
 }
--- src/components/Userprofile/UserDetails.jsx
+++ src/components/Userprofile/UserDetails.jsx
@@ -4,14 +4,14 @@ function UserDetails({ person, height, weight, setHeight, setWeight}) {
 
     const [isOverWeight, setIsOverWeight] = useState(false)
 
-    // //  runs on first render
-    // useEffect(() => {
-    //     console.log("I should arrive on the first render")
-    // }, [])
+    //  runs on first render
+    useEffect(() => {
+        console.log("I should arrive on the first render")
+    }, [])
 
     // runs when dependency array's state changes
     useEffect(() => {
-        console.log("weight", weight , "height", height)
+        console.log("weight", weight)
         if (weight > 90) {
             setIsOverWeight(true)
         } else {
@@ -26,18 +26,22 @@ function UserDetails({ person, height, weight, setHeight, setWeight}) {
         }
     }, [])
 
-    // const incrementWeightByTen = () => {
-    //     setWeight((prev) => prev + 10)
-    // }
+    const incrementWeightByTen = () => {
+        setWeight((prev) => prev + 10)
+    }
 
 
 
     return (
         <>
-
+            <button onClick={Child({incrementWeightByTen})}>Increment weight by 10</button>
+            <button onClick={() => setWeight(0)}>reset</button>
+            <button onClick={incrementHeightByTen}>Increment weight by 10</button>
+            <button onClick={() => setHeight(0)}>reset</button>
             <p> Full name {person.name}</p>
             <p> Age {person.age}</p>
-
+            <p> Height {height}</p>
+            <p> Weight {weight}</p>
             {isOverWeight ? <>you are overweight</> : <>You are not overweight</>}
         </>
     )
